e4963f4e146d3160dbc341660a0d55cc1a1efca7
SamanvithKashyap <61279872+SamanvithKashyap@users.noreply.github.com>
2020-07-18 20:15:43 +0530

    Add files via upload

---
+++ FLAPPY BIRDS/Planet.js
@@ -0,0 +1,23 @@
+class Planet{
+    constructor(x,y){
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x,y,40,40,options);
+        this.width = 40;
+        this.height = 40;
+        this.image = loadImage("./images/planet.png");
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        
+        imageMode(CENTER);
+        image(this.image,this.x,this.y,this.width,this.height);
+       
+        
+        
+    }
+};
